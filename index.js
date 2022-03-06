97774d38d8386cf0ecbec206340e6a5322278d93
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-03-06 22:06:38 +0300

    feat:extracted info from file

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
-function takevalue();
-var year= document.getElementById(year).value
-var month = document.getElementById(month).value
-var date = document.getElementById(date).value
+function getData()
+const userData = new FormData (form);
+const form = form;
+
+
+const buttonGen = document.querySelector(".generate")
+const genderInput = document.querySelectorAll(".radios")
+
+const datepicker = document.querySelector("#date").value;
+const year= document.querySelectorAll("year").value;
+const month = document.querySelector("month").value;
+
+
+const days= [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    
+]
+
+const akanFemalenames= [
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama",
+
+]
+const akanMalenames= [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame"
+
+]
+let day= datepicker.getDay();
