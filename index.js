c5837296e39afb633ae45869be988d06fa0f2bcf
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-03-08 00:05:17 +0300

    feat:added function

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const userData = document.querySelector("forms");
-const getAkan = document.getElementById("generate");
-var Pickdate =document.getElementById("date"). value;
-var year = document.getElementById("year") . value;
+
+function validate() {
+var Pickdate = document.getElementById("date").value;
+var dd = parseInt(Pickdate);
+var year = document.getElementById("year").value;
+var cc = year.substring(0,2);
+cc = parseInt(cc);
+var yy = year.substring(2,4)
+yy = parseInt(yy);
 var month = document.getElementById("month").value;
+var mm = parseInt(month);
+var genderInput = document.querySelector('input[name="gender"]:checked').value;
+var weekDay= Math.floor( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd )%7;
 
 
-let gender = userData.gender.value;
-const days= [
+
+const days=[
     "Sunday",
     "Monday",
     "Tuesday",
@@ -40,11 +48,16 @@ let akanMalenames= [
 
 
 var nameAkan;
-
-
-
-getAkan.addEventListener("click", (e) => {
-    e.preventDefault();
+if (genderInput=="female"){
+    alert("Your name is " + akanFemalenames[weekDay] + " because you were born on " + days[weekDay] )
 }
-)
+else if (genderInput=="male"){
+    alert("Your name is " + akanMalenames[weekDay] + " because you were born on " + days[weekDay] )
+
+    
+}
+
+
+}
+
 
